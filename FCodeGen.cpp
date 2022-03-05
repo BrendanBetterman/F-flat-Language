f62bad5287d75d26db543ab04f3f2cfce5cb1694
BrendanBetterman <58009876+BrendanBetterman@users.noreply.github.com>
2022-03-05 10:41:18 -0600

    inited scanner

--- FCodeGen.cpp
+++ FCodeGen.cpp
@@ -2,8 +2,13 @@
 
 using namespace std;
 
+extern ifstream sourceFile;
+extern ofstream outFile, listFile;
+
 #include "FScan.h"
 #include "FCodeGen.h"
+
+extern Scanner scan;
 
 CodeGen::CodeGen(){
     
